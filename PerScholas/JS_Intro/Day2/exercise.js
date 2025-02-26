b2b5d58166ca1269132a94ca99a1489929ed5ce1
Alecia Taylor <alecia.taylor0@gmail.com>
2025-02-26 11:30:30 -0500

    Add exercise.js for Day 2 exercises with conditional statements

---
+++ PerScholas/JS_Intro/Day2/exercise.js
@@ -0,0 +1,16 @@
+let num = 0
+
+if (num > 0) {
+    console.log("Positive!")
+} else {
+    console.log("Negative... So saaaad ...")
+}
+console.log(num)
+
+let age = 18
+
+if (age >= 18) {
+    console.log("You can party!")
+} else {
+    console.log("ACCESS DENIED!")
+}
