8f453f0bd3d9ea54f1b9c16c83ac0ebefd705862
Alecia Taylor <alecia.taylor0@gmail.com>
2025-02-26 22:33:51 -0500

    Age validation logic and enhance exercise.js with additional conditions and checks

--- PerScholas/JS_Intro/Day2/exercise.js
+++ PerScholas/JS_Intro/Day2/exercise.js
@@ -7,10 +7,65 @@ if (num > 0) {
 }
 console.log(num)
 
-let age = 18
+let age = 21
 
 if (age >= 18) {
-    console.log("You can party!")
+    if (age >= 21) {
+        console.log("You can party!")
+    } else {
+    console.log("You can party,but NO drinking!")
 } else {
-    console.log("ACCESS DENIED!")
+    console.log("ACCESS DENIED!Grow up!")
 }
+
+if (age >= 21) {
+    console.log("come on in and party!")
+}
+
+
+
+
+
+
+
+// exercise 3
+
+{let num = 5
+
+if (num > 0) { //checks if num is positive
+    if (num > 100) { //checks if num is greater than 100
+        console.log("Positive and greater than 100!")
+    } else {
+        console.log("Positive!")
+    } else {
+        console.log("Positive and less than 100")
+     } else {
+        console.log("Negative... So saaaad ...")
+     }
+    
+     let num2 = 100
+
+     if (num2 >= 100) {
+        console.log ("Positive and greate than or equal to 100") 
+    
+     } else if//checks if num is positive
+
+
+
+
+     // exercise 4
+
+     let grade = 65;
+
+if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 55) {
+  console.log("D");
+} else {
+  console.log("F");
+}g("D");
+        }
