ea6d57dbeae66ea3e291410de449dc652fcb20ce
Alecia Taylor <alecia.taylor0@gmail.com>
2025-02-28 11:48:29 -0500

    Introduce Exercise.js for arrays

---
+++ PerScholas/JS_Intro/Day4/Exercise.js
@@ -0,0 +1,11 @@
+//arrays start at 0
+//Organizaed by index
+//Key valued pairs (Technically)
+// More than one element
+//Arrays names should be plural
+//Are objects but function differently
+//Call them like variables
+
+let movies = ["MATILDA", "THE MATRIX", "LOTR"]
+let movie1 = movies[0]
+    console.log(movie1)
