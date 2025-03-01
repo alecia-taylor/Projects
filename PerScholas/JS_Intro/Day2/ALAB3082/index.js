129749e2800defd461f49ff2250b86befe878e47
Alecia Taylor <alecia.taylor0@gmail.com>
2025-03-01 01:03:16 -0500

    Enhance comments and add initial variables for plant growth calculations in index.js

--- PerScholas/JS_Intro/Day2/ALAB3082/index.js
+++ PerScholas/JS_Intro/Day2/ALAB3082/index.js
@@ -1,7 +1,11 @@
-//Advising a group of environmental scientists on how to handle the growth and spread of a unique plant species under their supervision
+//Advising a group of environmental scientists on how to handle the growth and spread of a unique plant species 
 
 // Part 1
   //Provided data
    const PI = 3.14; // The value of PI
-   
+   const area = PI * radius * radius; // The formula for the area of a circle
+   const growthRate =  *; // The growth rate of the plant
+
+   let numberOfPlants = 20; // The number of plants we started with
+   let week =
 
