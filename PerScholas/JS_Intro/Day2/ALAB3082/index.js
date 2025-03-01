de530f6ceeb035e61d76c6543deeb47b8b60b410
Alecia Taylor <alecia.taylor0@gmail.com>
2025-03-01 00:45:18 -0500

    Add initial index.js files for Day 3 and Day 4

--- PerScholas/JS_Intro/Day2/ALAB3082/index.js
+++ PerScholas/JS_Intro/Day2/ALAB3082/index.js
@@ -1,4 +1,7 @@
 //Advising a group of environmental scientists on how to handle the growth and spread of a unique plant species under their supervision
 
 // Part 1
+  //Provided data
+   const PI = 3.14; // The value of PI
+   
 
