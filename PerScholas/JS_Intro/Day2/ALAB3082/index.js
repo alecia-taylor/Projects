c5fe04e49ea44dff098cf6a5d0395e331ed7e822
Alecia Taylor <alecia.taylor0@gmail.com>
2025-03-01 10:25:03 -0500

    Add plant growth simulation for three weeks with space calculations

--- PerScholas/JS_Intro/Day2/ALAB3082/index.js
+++ PerScholas/JS_Intro/Day2/ALAB3082/index.js
@@ -7,17 +7,27 @@
    const area = PI * radius * radius; // The total area is 78.5 square feet
 
    let numberOfPlants = 20; // The number of plants we started with
-   let plantSpace = 0.8; // The required space for each plant
+   const plantSpace = 0.8; // The required space for each plant
    let numberOfWeeksPassed = 0; // The number of weeks that have passed
 
    console.log("Welcome to the Plant Growth Control System!");
    console.log("The garden has a total space of " + area + " square meters.");
 
+    // Calculate the maximum number of plants that can fit in the garden
+    while(numberOfWeeksPassed < 3) {    
+      numberOfWeeksPassed =  numberOfWeeksPassed + 1;
+      numberOfPlants = numberOfPlants * 2 // Plants double in size every week
+      console.log("--------" + numberOfWeeksPassed + " week(s) has passed. Here are the results--------");
+      console.log("Garden Space: " + area + " square feet"); 
+      console.log("Number of Weeks: " + numberOfWeeksPassed);
+      console.log("Number of plants " + numberOfPlants); 
+      console.log("Total Plant Space: " + plantSpace * numberOfPlants + " square feet");
+    }
    
+     // 3 weeks 
+     //console.log>=(area * 0.8) 
 
-// Implement control flow to make decisions on whether the plants should be:
-
-// Pruned, to stop them from exceeding the capacity of the garden.
+// Pruned, to stop the plants from exceeding the capacity of the garden.
 // This condition should be met if the plant count after 
 // the given number of weeks is greater than 80% of the maximum capacity of the garden.
 
