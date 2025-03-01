0c72bc88e9a6a35d7e46be186d87b77573f711e0
Alecia Taylor <alecia.taylor0@gmail.com>
2025-03-01 01:04:52 -0500

    Adjust growth rate assignment in index.js for plant growth calculations

--- PerScholas/JS_Intro/Day2/ALAB3082/index.js
+++ PerScholas/JS_Intro/Day2/ALAB3082/index.js
@@ -4,8 +4,7 @@
   //Provided data
    const PI = 3.14; // The value of PI
    const area = PI * radius * radius; // The formula for the area of a circle
-   const growthRate =  *; // The growth rate of the plant
+   const growthRate = numberOfweeks; // The growth rate of the plant
 
    let numberOfPlants = 20; // The number of plants we started with
-   let week =
 
