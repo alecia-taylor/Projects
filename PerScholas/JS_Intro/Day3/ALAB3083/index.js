b91a8527b9498eac796b6408f664254f062db8f9
Alecia Taylor <alecia.taylor0@gmail.com>
2025-03-04 12:53:02 -0500

    Add CSV data and split into rows for Part 3: Feeling Loopy

--- PerScholas/JS_Intro/Day3/ALAB3083/index.js
+++ PerScholas/JS_Intro/Day3/ALAB3083/index.js
@@ -64,3 +64,8 @@ let n = 4; // Change this number to test different cases
 nextPrime(n); // This will find and print the next prime number
 
 // Part 3: Feeling Loopy
+ let data = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+ // Split the data into rows
+ let rows = data.split("\n");
+
