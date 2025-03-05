3675e140209d7c7609b501035ebef71de6959086
Alecia Taylor <alecia.taylor0@gmail.com>
2025-03-05 10:29:31 -0500

    Add CSV data handling and implement basic functions for area computation and even number check

--- Assignments/JS_Intro/Day4/ALAB3084/index.js
+++ Assignments/JS_Intro/Day4/ALAB3084/index.js
@@ -1,8 +1,17 @@
 //Part 1
  // Refer to “Part 3.” <String filePath = C:\Users\Zaire\Desktop\dev\alecia-taylor\PerScholas\JS_Intro\Day3\ALAB3083\instructions.md>
  //Provided CSV data: ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
- const category = [ ID, Named, Occupation, Age];
-    console.log (data);
+ const category = [ "ID", "Named", "Occupation", "Age"];
+    console.log (category);
 
- const Bruce = [ Bruce, Knight, 41];
-    console.log (Bruce);
+ const bruce = [ "42", "Bruce", "Knight", 41];
+    console.log (bruce);
+
+ const bob = [ "57", "Bob", "Fry Cook", 19]
+    console.log (bob);
+
+ const blaine = ["63",  "Blaine", "Quiz Master", 58]          
+    console.log (blaine);                                                             
+
+ const bill = [ "98", "Bill", "Doctor’s Assistant", 26]
+    console.log (bill);
