e4e29bcaa29c90997e4dc0c92071fe5a61e50ee2
Alecia Taylor <alecia.taylor0@gmail.com>
2025-03-05 02:12:32 -0500

    Update README with refactoring instructions, add CSV data handling in index.js, and fix link typo in index.html

--- Assignments/JS_Intro/Day4/ALAB3084/index.js
+++ Assignments/JS_Intro/Day4/ALAB3084/index.js
@@ -1,3 +1,8 @@
 //Part 1
  // Refer to “Part 3.” <String filePath = C:\Users\Zaire\Desktop\dev\alecia-taylor\PerScholas\JS_Intro\Day3\ALAB3083\instructions.md>
- 
+ //Provided CSV data: ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
+ const category = [ ID, Named, Occupation, Age];
+    console.log (data);
+
+ const Bruce = [ Bruce, Knight, 41];
+    console.log (Bruce);
